5569078eb0159330e66c5167c6f20eb777ac9a24
gyozpada <galanglost@gmail.com>
2023-08-10 16:12:19 +0700

    update db

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"api-gin/models"
+	"api-gin/utils"
 	"fmt"
 
 	"gorm.io/driver/mysql"
@@ -9,12 +10,13 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	username := "root"
-	password := "password"
-	host := "tcp(127.0.0.1:3306)"
-	database := "database_movie"
+	username := utils.Getenv("DATABASE_USERNAME", "root")
+	password := utils.Getenv("DATABASE_PASSWORD", "password")
+	host := utils.Getenv("DATABASE_HOST", "127.0.0.1")
+	port := utils.Getenv("DATABASE_PORT", "3306")
+	database := utils.Getenv("DATABASE_NAME", "database_movie")
 
-	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
